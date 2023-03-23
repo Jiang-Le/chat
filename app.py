fd9bce634d883b0a923469b5361deb9fb4160557
lejiang <lejiang@tencent.com>
2023-03-23 14:58:15 +0800

    optimize ui

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import openai
 
+def format_user_prompt(text):
+    return f'<p style="background-color:#F0F2F6;padding: 8px;border-radius: 5px"><span style="font-weight:bold">Question</span>: {text}</p>'
+
 def add_textbox(key, content):
-    st.text_input(key, content)
+    st.markdown(content, unsafe_allow_html=True)
 
 def add_text_area(key, content):
     st.markdown(content)
@@ -20,23 +23,31 @@ if "messages" not in st.session_state:
 
 openai.api_key = st.text_input("Paste your OpenAI API Key here", value="", type="password")
 
+for i in range(st.session_state.num_textboxes):
+    context = st.session_state.context[i]
+    add_textbox(f"Question{i}", format_user_prompt(context["user_prompt"]))
+    add_text_area(f"Answer{i}", context["answer"])
+
+place_holder = st.empty()
+place_holder2 = st.empty()
+
 prompt = st.text_input("Prompt")
 
 if st.button('Send'):
     st.markdown('---')
-    st.session_state.num_textboxes += 1
     with st.spinner("Generating response..."):
-        st.session_state["messages"] += [{"role": "user", "content": prompt}]
+        messages = st.session_state["messages"]
+        messages += [{"role": "user", "content": prompt}]
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",messages=st.session_state["messages"]
+            model="gpt-3.5-turbo",messages=messages
         )
         message_response = response["choices"][0]["message"]["content"]
         st.session_state.context.append({
             "user_prompt": prompt,
             "answer": message_response
         })
-
-for i in range(st.session_state.num_textboxes):
-    context = st.session_state.context[i]
-    add_textbox(f"Question{i}", context["user_prompt"])
-    add_text_area(f"Answer{i}", context["answer"])
+        st.session_state["messages"] = messages
+        
+        st.session_state.num_textboxes += 1
+        place_holder.markdown(format_user_prompt(prompt), unsafe_allow_html=True)
+        place_holder2.markdown(message_response)
